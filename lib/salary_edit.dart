4a73655c915d276583c7ee80da84e32c6c1c1bba
James Finlay <jtfinlay@ualberta.ca>
2017-10-21 19:31:47 -0700

    edit_salary widget layout done

--- lib/salary_edit.dart
+++ lib/salary_edit.dart
@@ -80,6 +80,105 @@ class SalaryEditWidgetState extends State<SalaryEditWidget> {
           )
         ]
       ),
+      body: new ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: <Widget>[
+          new TextField(
+            onSubmitted: null,
+            decoration: new InputDecoration(
+              icon: new Icon(Icons.work),
+              hintText: 'Name of employer or your position',
+              labelText: 'Job Title *',
+            ),
+          ),
+          new TextField(
+            onSubmitted: null,
+            keyboardType: TextInputType.number,
+            decoration: new InputDecoration(
+              labelText: 'Salary (Hourly) *',
+              prefixText: '\$',
+              suffixText: 'USD',
+              suffixStyle: const TextStyle(color: Colors.green)
+            ),
+          ),
+          new Padding(padding: const EdgeInsets.only(top: 20.0)),
+          new Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                new Text(
+                  'Start Time (optional):',
+                  style: Theme.of(context).textTheme.subhead
+                ),
+                new Expanded(
+                  child: new Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      new FlatButton(
+                        child: new Text(
+                          'SET',
+                          style: Theme.of(context).textTheme.button.copyWith(
+                            fontSize: 20.0
+                          )
+                        ),
+                        onPressed: () { },
+                        )
+                    ],
+                  )
+                ),
+              ],
+          ),
+          new Padding(padding: const EdgeInsets.only(top: 10.0)),
+          new Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              new Text(
+                'End Time (optional):',
+                style: Theme.of(context).textTheme.subhead
+              ),
+              new Expanded(
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    new FlatButton(
+                      child: new Text(
+                       'SET',
+                        style: Theme.of(context).textTheme.button.copyWith(
+                          fontSize: 20.0
+                        )
+                      ),
+                      onPressed: () { },
+                    )
+                  ],
+                )
+              ),
+            ],
+          ),
+          new Padding(padding: const EdgeInsets.only(top: 20.0)),
+          new Text(
+            'Week days (optional)',
+            style: Theme.of(context).textTheme.subhead
+          ),
+          new Padding(padding: const EdgeInsets.only(top: 20.0)),
+          new Row(
+            children: <Widget>[
+              new Padding(padding: const EdgeInsets.only(left:20.0)),
+              new Expanded( child: new Text('S') ),
+              new Expanded( child: new Text('M') ),
+              new Expanded( child: new Text('T') ),
+              new Expanded( child: new Text('W') ),
+              new Expanded( child: new Text('T') ),
+              new Expanded( child: new Text('F') ),
+              new Expanded( child: new Text('S') ),
+              new Padding(padding: const EdgeInsets.only(right:20.0)),
+            ],
+          ),
+          new Container(
+            padding: const EdgeInsets.only(top: 40.0),
+            child: new Text('* indicated required field',
+              style: Theme.of(context).textTheme.caption)
+          )
+        ]
+      )
     );
   }
 
