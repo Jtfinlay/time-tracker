d0dfa8cf81716b83c6b44542eaf55f3b2176c957
James Finlay <jtfinlay@ualberta.ca>
2017-10-22 23:33:39 -0700

    validates input before save

--- lib/salary_edit.dart
+++ lib/salary_edit.dart
@@ -8,6 +8,21 @@ enum DismissDialogAction {
   save,
 }
 
+class JobData {
+  String title = '';
+  String salary = '';
+  TimeOfDay startTime, endTime;
+  Map<String,bool> weekDays = {
+    "Sunday": false,
+    "Monday": false,
+    "Tuesday": false,
+    "Wednesday": false,
+    "Thursday": false,
+    "Friday": false,
+    "Saturday": false,
+  };
+}
+
 class _InputDropdown extends StatelessWidget {
   const _InputDropdown({
   Key key,
@@ -114,26 +129,19 @@ class SalaryEditWidget extends StatefulWidget {
 }
 
 class SalaryEditWidgetState extends State<SalaryEditWidget> {
-  TimeOfDay _startTime, _endTime;
-  String _title, _salary;
-  bool _savedNeeded = false;
-  Map<String, bool> _weekDays;
+  bool _autovalidate = false;
+  bool _formWasEdited = false;
+  JobData job = null;
 
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
 
   @override
   void initState() {
     super.initState();
 
     // TODO - Check settings if editing existing value
-    _weekDays = {
-      "Sunday": false,
-      "Monday": false,
-      "Tuesday": false,
-      "Wednesday": false,
-      "Thursday": false,
-      "Friday": false,
-      "Saturday": false,
-    };
+    job = new JobData();
   }
 
   String validateName(String value) {
@@ -144,43 +152,41 @@ class SalaryEditWidgetState extends State<SalaryEditWidget> {
     return null;
   }
 
-  void handleTitleChanged(String title) {
-    if (title.isNotEmpty && title != _title) {
-      _title = title;
-      _savedNeeded = true;
-    }
-  }
-
-  void handleSalaryChanged(String salary) {
-    if (salary.isNotEmpty && salary != _salary) {
-      _salary = salary;
-      _savedNeeded = true;
-    }
-  }
-
   void handleWeekDayItemPressed(String weekday) {
     setState(() {
-      _savedNeeded = true;
-      _weekDays[weekday] = !_weekDays[weekday];
+      _formWasEdited = true;
+      job.weekDays[weekday] = !job.weekDays[weekday];
 
-      if (_startTime == null) {
-        _startTime = new TimeOfDay(hour: 0, minute: 0);
+      if (job.startTime == null) {
+        job.startTime = new TimeOfDay(hour: 0, minute: 0);
       }
-      if (_endTime == null) {
-        _endTime = new TimeOfDay(hour:23, minute: 59);
+      if (job.endTime == null) {
+        job.endTime = new TimeOfDay(hour:23, minute: 59);
       }
 
     });
   }
 
-  void handleSaveAndDismiss(BuildContext context) {
-//    if (title)
+  void showInSnackBar(String value) {
+    _scaffoldKey.currentState.showSnackBar(new SnackBar(
+        content: new Text(value)
+    ));
+  }
 
-    Navigator.pop(context, DismissDialogAction.save);
+  void handleSaveAndDismiss(BuildContext context) {
+    final FormState form = _formKey.currentState;
+    if (!form.validate()) {
+      _autovalidate = true;
+      showInSnackBar('Please fix the errors in red before submitting.');
+    } else {
+      form.save();
+      showInSnackBar('${job.title} has been added');
+      Navigator.pop(context, DismissDialogAction.save);
+    }
   }
 
   void handleDismissButton(BuildContext context) {
-    if (!_savedNeeded) {
+    if (!_formWasEdited) {
       Navigator.pop(context, null);
       return;
     }
@@ -218,7 +224,7 @@ class SalaryEditWidgetState extends State<SalaryEditWidget> {
   Widget buildWeekDayList(BuildContext context) {
     var children = <Widget>[new Padding(padding: const EdgeInsets.only(left:20.0))];
 
-    _weekDays.forEach((name, enabled) =>
+    job.weekDays.forEach((name, enabled) =>
       children.add(new _WeekDayItem(
         labelText: name[0],
         enabled: enabled,
@@ -229,11 +235,29 @@ class SalaryEditWidgetState extends State<SalaryEditWidget> {
     return new Row(children: children);
   }
 
+  String _validateName(String value) {
+    _formWasEdited = true;
+    if (value.isEmpty) {
+      return 'Job title is required.';
+    }
+    return null;
+  }
+
+  String _validateSalary(String value) {
+    _formWasEdited = true;
+    if (value.isEmpty) {
+      return 'Salary is required.';
+    }
+    // TODO - Regex validation
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
 
     return new Scaffold(
+      key: _scaffoldKey,
       appBar: new AppBar(
         leading: new IconButton(
           icon: new Icon(Icons.clear),
@@ -249,69 +273,75 @@ class SalaryEditWidgetState extends State<SalaryEditWidget> {
           )
         ]
       ),
-      body: new ListView(
-        padding: const EdgeInsets.all(16.0),
-        children: <Widget>[
-          new TextField(
-            onChanged: handleTitleChanged,
-            decoration: new InputDecoration(
-              icon: new Icon(Icons.work),
-              hintText: 'Name of employer or your position',
-              labelText: 'Job Title *',
+      body: new Form(
+        key: _formKey,
+        autovalidate: _autovalidate,
+        child: new ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          children: <Widget>[
+            new TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.work),
+                hintText: 'Name of employer or your position',
+                labelText: 'Job Title *',
+              ),
+              onSaved: (String value) { job.title = value; },
+              validator: _validateName,
             ),
-          ),
-          new TextField(
-            onChanged: handleSalaryChanged,
-            keyboardType: TextInputType.number,
-            decoration: new InputDecoration(
-              labelText: 'Salary (Hourly) *',
-              prefixText: '\$',
-              suffixText: 'USD',
-              suffixStyle: const TextStyle(color: Colors.green)
+            new TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Salary (Hourly) *',
+                prefixText: '\$',
+                suffixText: 'USD',
+                suffixStyle: const TextStyle(color: Colors.green)
+              ),
+              keyboardType: TextInputType.number,
+              onSaved: (String value) { job.salary = value; },
+              validator: _validateSalary
             ),
-          ),
 
-          new Padding(padding: const EdgeInsets.only(top: 20.0)),
-          new Row(
+            new Padding(padding: const EdgeInsets.only(top: 20.0)),
+            new Row(
               children: <Widget>[
                 new _TimePicker(
                     labelText: 'Start Time (optional):',
-                    selectedTime: _startTime,
+                    selectedTime: job.startTime,
                     selectTime: (TimeOfDay t) {
                       setState(() {
-                        _startTime = t;
-                        _savedNeeded = true;
+                        _formWasEdited = true;
+                        job.startTime = t;
                       });
                     }
                 ),
                 const SizedBox(width: 12.0),
                 new _TimePicker(
                     labelText: 'End Time (optional):',
-                    selectedTime: _endTime,
+                    selectedTime: job.endTime,
                     selectTime: (TimeOfDay t) {
                       setState(() {
-                        _endTime = t;
-                        _savedNeeded = true;
+                        _formWasEdited = true;
+                        job.endTime = t;
                       });
                     }
                 ),
               ],
-          ),
+            ),
 
-          new Padding(padding: const EdgeInsets.only(top: 20.0)),
-          new Text(
-            'Week days (optional)',
-            style: Theme.of(context).textTheme.subhead
-          ),
-          new Padding(padding: const EdgeInsets.only(top: 20.0)),
-          buildWeekDayList(context),
+            new Padding(padding: const EdgeInsets.only(top: 20.0)),
+            new Text(
+                'Week days (optional)',
+                style: Theme.of(context).textTheme.subhead
+            ),
+            new Padding(padding: const EdgeInsets.only(top: 20.0)),
+            buildWeekDayList(context),
 
-          new Container(
-            padding: const EdgeInsets.only(top: 40.0),
-            child: new Text('* indicates required field',
-              style: Theme.of(context).textTheme.caption)
-          )
-        ]
+            new Container(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: new Text('* indicates required field',
+                    style: Theme.of(context).textTheme.caption)
+            )
+          ]
+        )
       )
     );
   }
